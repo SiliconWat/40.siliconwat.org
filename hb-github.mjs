2c82af529f667ae75e49c8a4b24bcf9c66ae6ae7
Thon Ly <thonly@heartbank.ceo>
2020-09-18 13:11:02 -0700

    fix hb-github scope

--- hb-github.mjs
+++ hb-github.mjs
@@ -7,13 +7,13 @@ template.innerHTML = `
         ::slotted(img) {
             width: 50px;
         }
-        ::slotted(button) {
+        ::slotted(img, button) {
             display: none;
         }
     </style>
     <slot></slot>
 `
-
+// https://developer.github.com/v4/explorer/
 const QUERY = `{"query":
 "query { \
     viewer { \
@@ -21,6 +21,7 @@ const QUERY = `{"query":
         nodes { \
           tier { \
             id \
+            monthlyPriceInDollars \
             description \
           } \
         } \
@@ -35,32 +36,28 @@ export class HbGithub extends HTMLElement{
         this.attachShadow({mode: "open"})
         this.shadowRoot.appendChild(template.content.cloneNode(true))
 
+        this.img = this.querySelector("img")
+        this.span = this.querySelector("span")
+        this.loginButton = this.querySelector("button:nth-child(3)")
+        this.logoutButton = this.querySelector("button:nth-child(4)")
+
         this.login = this.login.bind(this)
         this.logout = this.logout.bind(this)
     }
 
-    connectedCallback() {
-        const img = this.querySelector("img")
-        const span = this.querySelector("span")
-        const p = this.querySelector("p")
-        const login = this.querySelector("button:nth-child(3)")
-        const logout = this.querySelector("button:nth-child(4)")
-        login.addEventListener("click", this.login)
-        logout.addEventListener("click", this.logout)
+    connectedCallback() {    
+        this.loginButton.addEventListener("click", this.login)
+        this.logoutButton.addEventListener("click", this.logout)
 
         window.firebase.auth().onAuthStateChanged(user => {
+            this.loginButton.style.display = "none"
+            this.logoutButton.style.display = "none"
             if (user) {
-                login.style.display = "none"
-                logout.style.display = "inline"
-                const profile = user.providerData[0]
-                img.src = profile.photoURL
-                span.textContent = profile.displayName
+                this.logoutButton.style.display = "inline"
+                this.showProfile(user.providerData[0])
             } else {
-                login.style.display = "inline"
-                logout.style.display = "none"
-                img.src = ""
-                span.textContent = ""
-                p.textContent = ""
+                this.loginButton.style.display = "inline"
+                this.hideProfile()
             }
         })
 
@@ -68,24 +65,40 @@ export class HbGithub extends HTMLElement{
         .then(result => fetch("https://api.github.com/graphql", {method: "POST", headers: {Authorization: `Bearer ${result.credential.accessToken}`}, body: QUERY})) //console.log(result.user)
         .then(response => response.json())
         .then(json => {
-            console.log(json.data.viewer.sponsorshipsAsSponsor.nodes)
+            //console.log(json.data.viewer.sponsorshipsAsSponsor.nodes)
             if (json.data.viewer.sponsorshipsAsSponsor.nodes.some(sponorships => sponorships.tier.id === "MDEyOlNwb25zb3JzVGllcjQxODk1")) {
-                window.location.href = "medium.html"
+                window.location.href = this.getAttribute("success-page")
             } else {
-                p.textContent = "Please donate to see 40..."
+                this.querySelector("p").textContent = "Please donate to see 40..."
             }
         })
-        .catch(error => p.textContent = error.message)
+        .catch(error => console.log(error.message))
     }
 
     login(event) {
-        const provider = new window.firebase.auth.GithubAuthProvider();
-        window.firebase.auth().signInWithRedirect(provider);
+        this.loginButton.disabled = true
+        const provider = new window.firebase.auth.GithubAuthProvider()
+        provider.addScope("user")
+        provider.setCustomParameters({allow_signup: true})
+        window.firebase.auth().signInWithRedirect(provider)
     }
 
     logout(event) {
+        this.logoutButton.disabled = true
+
         window.firebase.auth().signOut()
-        .then(() => console.log("signed out of github"))
+        .then(() => window.location.href = this.getAttribute("login-page"))
         .catch(error => console.error(error.message));
+    }
+
+    showProfile(profile) {
+        this.img.src = profile.photoURL
+        this.img.style.display = "inline"
+        this.span.textContent = profile.displayName
+    }
+
+    hideProfile() {
+        this.img.style.display = "none"
+        this.span.textContent = ""
     }
 }
